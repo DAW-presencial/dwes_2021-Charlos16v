d555188aae81d9c93be544b24c385ba5d36c3544
Carlos Uriel Domínguez Ruiz-Díaz <cdominguez@cifpfbmoll.eu>
2021-11-29 15:54:22 +0100

    feat: answer question 3

---
+++ 3.php
@@ -0,0 +1,11 @@
+<?php 
+    // PHP_INT_MAX es una CONSTANTE PREDEFINIDA, dependiendo del sistema de bits (32 o 64),
+    // del equipo en el que estamos usando la constante nos devolvera un valor u otro.
+    // Normalmente int(2147483647) en sistemas de 32 bits y int(9223372036854775807) en sistemas de 64 bits.
+    // ejecutamos fichero: php 3.php
+
+    // Mostramos el valor de la constante con usando echo().
+        echo(PHP_INT_MAX);
+
+?>
+
